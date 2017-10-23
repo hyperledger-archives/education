1ddf5d53af7fbb420d03ce5206a006353439992d
Alexandra <alexandrag2254@gmail.com>
2017-10-23 15:28:52 -0700

    adding LFS171x sawtooth code to education folder (#6)
    * adding LFS171x sawtooth code to education folder
    
    Signed-off-by: Alexandra Groetsema <alexandrag2254@gmail.com>

---
+++ LFS171x/sawtooth-material/sawtooth-tuna/client/src/components.js
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+
+'use strict'
+
+const $ = require('jquery')
+
+// Add select option which may be set to selected
+const addOption = (parent, value, selected = false) => {
+  const selectTag = selected ? ' selected' : ''
+  $(parent).append(`<option value="${value}"${selectTag}>${value}</option>`)
+}
+
+// Add a new table row with any number of cells
+const addRow = (parent, ...cells) => {
+  const tds = cells.map(cell => `<td>${cell}</td>`).join('')
+  $(parent).append(`<tr>${tds}</tr>`)
+}
+
+// Add div with accept/reject buttons
+const addAction = (parent, label, action) => {
+  $(parent).append(`<div>
+  <span>${label}</span>
+  <input class="accept btn btn-primary" type="button" value="Accept">
+  <input class="reject btn btn-caution" type="button" value="Reject">
+</div>`)
+}
+
+module.exports = {
+  addOption,
+  addRow,
+  addAction
+}
---
+++ LFS171x/sawtooth-material/sawtooth-tuna/processor/index.js
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+
+'use strict'
+
+const { TransactionProcessor } = require('sawtooth-sdk/processor')
+const { JSONHandler } = require('./handlers')
+
+const VALIDATOR_URL = 'tcp://localhost:4004'
+
+// Initialize Transaction Processor
+const tp = new TransactionProcessor(VALIDATOR_URL)
+tp.addHandler(new JSONHandler())
+tp.start()
