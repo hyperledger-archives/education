5a33ac0c03a72f51157dc4b6995975f02b0d1c73
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2018-07-16 14:24:03 +0300

    HC: Added models CTO file, JS logic and Queries for Composer tuna network
    Signed-off-by: Alejandro Vicente Grabovetsky <sasha@aid.technology>

---
+++ LFS171x/composer-material/tuna-network/lib/logic.js
@@ -0,0 +1,42 @@
+'use strict';
+/**
+ * Defining the namespace for the business network
+ */
+const NS = 'org.tuna';
+
+/**
+ * Transfer tuna from one owner to another
+ * @param {org.tuna.SellTuna} tx - The transferTuna transaction
+ * @transaction
+ */
+async function sellTuna(tx) {
+    // Get asset registry for Tuna
+    const tunaRegistry = await getAssetRegistry(NS + '.Tuna');
+
+    // Make sure the tuna status is CAUGHT and not PURCHASED
+    if (tx.tuna.status !== 'CAUGHT') {
+        throw new Error(`Tuna with id ${tx.tuna.getIdentifier()} is not in CAUGHT status`);
+    }
+
+    // Get participant registry for Individuals
+    const restaurantOwnerRegistry = await getParticipantRegistry(NS + '.RestaurantOwner');
+
+    // Get newOwner
+    const newOwnerID = tx.newOwner.getIdentifier();
+    const oldOwnerID = tx.tuna.owner.getIdentifier();
+
+    // Make sure that new owner exists
+    const newOwner = await restaurantOwnerRegistry.get(newOwnerID);
+
+    // Check that newOwner is not same as current owner
+    if (newOwnerID === oldOwnerID) {
+        throw new Error(`Tuna with id ${tx.tuna.getIdentifier()} is already owned by ${oldOwnerID}`);
+    }
+
+    // Update tuna with new owner
+    tx.tuna.owner = tx.newOwner;
+    tx.tuna.status = "PURCHASED";
+
+    // Update the asset in the asset registry.
+    await tunaRegistry.update(tx.tuna);
+}
