0cd4a3282da6f90f968bccc7b68d5d7ab59a755d
Nicola Paoli <nicola@aid.technology>
2018-08-02 16:18:25 +0100

    [ADD-COMPOSER]: Refactoring logic to use same structure as Text sections
    Signed-off-by: Nicola Paoli <nicola@aid.technology>

--- LFS171x/composer-material/tuna-network/lib/logic.js
+++ LFS171x/composer-material/tuna-network/lib/logic.js
@@ -10,27 +10,33 @@ const NS = 'org.tuna';
  * @transaction
  */
 async function sellTuna(tx) {
+
     // Get asset registry for Tuna
     const tunaRegistry = await getAssetRegistry(NS + '.Tuna');
+
+    // Get participant registry for Individuals
+    const restaurantOwnerRegistry = await getParticipantRegistry(NS + '.RestaurantOwner');
 
     // Make sure the tuna status is CAUGHT and not PURCHASED
     if (tx.tuna.status !== 'CAUGHT') {
         throw new Error(`Tuna with id ${tx.tuna.getIdentifier()} is not in CAUGHT status`);
     }
 
-    // Get participant registry for Individuals
-    const restaurantOwnerRegistry = await getParticipantRegistry(NS + '.RestaurantOwner');
-
-    // Get newOwner
+    // Get newOwner ID
     const newOwnerID = tx.newOwner.getIdentifier();
-    const oldOwnerID = tx.tuna.owner.getIdentifier();
 
-    // Make sure that new owner exists
-    const newOwner = await restaurantOwnerRegistry.get(newOwnerID);
+    // Get current Owner ID
+    const oldOwnerID = tx.tuna.owner.getIdentifier();
 
     // Check that newOwner is not same as current owner
     if (newOwnerID === oldOwnerID) {
         throw new Error(`Tuna with id ${tx.tuna.getIdentifier()} is already owned by ${oldOwnerID}`);
+    }
+
+    // Make sure that new owner exists
+    const newOwner = await restaurantOwnerRegistry.get(newOwnerID);
+    if (!newOwner) {
+        throw new Error(`RestaurantOwner with id ${newOwnerID} does not exist`);
     }
 
     // Update tuna with new owner
